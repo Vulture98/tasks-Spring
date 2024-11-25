aaf0bc91cc8564ab18e64489f475ef2d9853b2d7
bharath <bh814281@gmail.com>
2024-11-25 09:05:46 +0530

    initial commit

---
+++ src/test/java/com/featuring/tasks/firstProject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featuring.tasks.firstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
